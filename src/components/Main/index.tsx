98e058e8b963b0c6953cb87a3434ebbdd98172e5
Marcelo Oliveira <marcelolynx@gmail.com>
2020-09-02 20:51:38 -0400

    criando os primeiros esilos com styled-component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title>React avançado</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="imagem de um desenvolvedor em frente a uma tela com códigos"
+    />
+  </S.Wrapper>
 )
 
 export default Main
---
+++ src/components/Main/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  background-color: #06092b;
+  color: #fff;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  padding: 3rem;
+`
+export const Logo = styled.img``
+
+export const Title = styled.h1``
+
+export const Description = styled.h2``
+
+export const Illustration = styled.img``
