a7659c7230d9dfb3a2338c57b441ae13dd6a3b9b
Marcelo Oliveira <marcelolynx@gmail.com>
2020-09-02 20:19:44 -0400

    finalizando as configurações do styledcomponents no projeto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,5 @@
-import Head from 'next/head'
+import Main from 'components/Main'
 
-export default function Home({ title = 'React Avançado ' }) {
-  return (
-    <div className="container">
-      <Head>
-        <title>{title}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="title">
-          React Avançado com <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
-
-      <footer className="footer">
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-    </div>
-  )
+export default function Home() {
+  return <Main />
 }
