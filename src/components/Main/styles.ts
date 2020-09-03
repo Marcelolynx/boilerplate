6d525f9c4b66c2774af023c1525ebefc7a5da50b
Marcelo Oliveira <marcelolynx@gmail.com>
2020-09-03 16:21:31 -0400

    Adicionando as configurações do storybook ao boilerplate + Knobs

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -12,10 +12,21 @@ export const Wrapper = styled.main`
   justify-content: center;
   padding: 3rem;
 `
-export const Logo = styled.img``
+export const Logo = styled.img`
+  width: 25rem;
+  margin-bottom: 2rem;
+`
 
-export const Title = styled.h1``
+export const Title = styled.h1`
+  font-size: 2.5rem;
+`
 
-export const Description = styled.h2``
+export const Description = styled.h2`
+  font-size: 2rem;
+  font-weight: 400;
+`
 
-export const Illustration = styled.img``
+export const Illustration = styled.img`
+  margin-top: 3rem;
+  width: min(30rem, 100%);
+`
