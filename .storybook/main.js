e91a030a372bc909df7f480eee12a18fef35ef95
Marcelo Oliveira <marcelolynx@gmail.com>
2020-09-03 17:57:48 -0400

    criando suporte a PWA

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,10 +3,5 @@ module.exports = {
     "../src/components/**/stories.tsx",
     "../src/components/**/stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-knobs/register"
-
-  ]
+  "addons": ["@storybook/addon-essentials"]
 }
--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,10 +1,19 @@
-import { withKnobs, text } from '@storybook/addon-knobs'
+import { Story, Meta } from '@storybook/react/types-6-0'
 import Main from '.'
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs]
-}
+  args: {
+    title: 'title default',
+    description: 'description default'
+  }
+} as Meta
 
-export const Basic = () => <Main title={text('Title', 'React Avançado')} />
+export const Basic: Story = (args) => <Main {...args} />
+
+export const Extra: Story = (args) => <Main {...args} />
+Extra.args = {
+  title: 'Kluser Tecnologia',
+  description: 'JavaScript'
+}
